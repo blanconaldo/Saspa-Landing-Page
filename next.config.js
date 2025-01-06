d844d8d1ebdd1fbc55c512938c33b6eae2e4f631
Youssef Mohamed <122948264+blanconaldo@users.noreply.github.com>
2025-01-06 23:53:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'd.media.kavehome.com',
-        'www.organizedinteriors.com',
-        'rigstudio.international',
-        'www.nismaayadecor.in',
-        'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
+  images: {
+    domains: [
+      'd.media.kavehome.com',
+      'www.organizedinteriors.com',
+      'rigstudio.international',
+      'www.nismaayadecor.in',
+      'hebbkxlanhila5yf.public.blob.vercel-storage.com',
+    ],
+  },
+  output: 'standalone',  // Add this line
+}
+
+module.exports = nextConfig
